db052e3541fd04936b175627373aa7b318d375d9
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-09-05 15:22:19 +0100

    remove package name

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* jshint esversion: 9, -W030, node: true */
 "use strict";
-const constants = require("./lib/constants");
 module.exports = function (homebridge) {
    let eWeLink = require("./lib/eWeLink.js")(homebridge);
-   homebridge.registerPlatform(constants.packageName, "eWeLink", eWeLink, true);
+   homebridge.registerPlatform("homebridge-ewelink", "eWeLink", eWeLink, true);
 };
