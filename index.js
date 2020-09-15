bb1be253518800f38ff67aab154e10b45bc928d6
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-09-15 16:25:56 +0100

    prettier code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 9, -W030, node: true */
 "use strict";
 module.exports = function (homebridge) {
-   const eWeLink = require("./lib/eWeLink.js")(homebridge);
-   homebridge.registerPlatform("homebridge-ewelink", "eWeLink", eWeLink, true);
+  const eWeLink = require("./lib/eWeLink.js")(homebridge);
+  homebridge.registerPlatform("homebridge-ewelink", "eWeLink", eWeLink, true);
 };
