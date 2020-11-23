972bd62d8c072e31f45261f6c253c078a831a4d7
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-11-23 15:58:53 +0000

    custom-ui updates

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -5,25 +5,29 @@ class PluginUiServer extends HomebridgePluginUiServer {
   constructor () {
     super()
     this.onRequest('/getCachedAccessories', async () => {
-      const devicesToReturn = []
-      let cachedAccessories = await fs.promises.readFile(this.homebridgeStoragePath + '/accessories/cachedAccessories')
-      cachedAccessories = JSON.parse(cachedAccessories)
-      cachedAccessories
-        .filter(accessory => accessory.plugin === 'homebridge-ewelink')
-        .sort((a, b) => {
-          return a.displayName.toLowerCase() > b.displayName.toLowerCase()
-            ? 1
-            : b.displayName.toLowerCase() > a.displayName.toLowerCase()
-              ? -1
-              : 0
-        })
-        .forEach(accessory => {
-          devicesToReturn.push({
-            displayName: accessory.displayName,
-            context: accessory.context
+      try {
+        const devicesToReturn = []
+        let cachedAccessories = await fs.promises.readFile(this.homebridgeStoragePath + '/accessories/cachedAccessories')
+        cachedAccessories = JSON.parse(cachedAccessories)
+        cachedAccessories
+          .filter(accessory => accessory.plugin === 'homebridge-ewelink')
+          .sort((a, b) => {
+            return a.displayName.toLowerCase() > b.displayName.toLowerCase()
+              ? 1
+              : b.displayName.toLowerCase() > a.displayName.toLowerCase()
+                ? -1
+                : 0
           })
-        })
-      return devicesToReturn
+          .forEach(accessory => {
+            devicesToReturn.push({
+              displayName: accessory.displayName,
+              context: accessory.context
+            })
+          })
+        return devicesToReturn
+      } catch {
+        return []
+      }
     })
     this.ready()
   }
