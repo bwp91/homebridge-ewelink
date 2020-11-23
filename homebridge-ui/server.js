a42600540db8e9ffeca4a24711adddab0c670386
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-11-23 13:53:26 +0000

    snazzy little ui

---
+++ homebridge-ui/server.js
@@ -0,0 +1,34 @@
+const { HomebridgePluginUiServer } = require('@homebridge/plugin-ui-utils')
+const fs = require('fs')
+
+class PluginUiServer extends HomebridgePluginUiServer {
+  constructor () {
+    super()
+    this.onRequest('/getCachedAccessories', async () => {
+      const devicesToReturn = []
+      let cachedAccessories = await fs.promises.readFile(this.homebridgeStoragePath + '/accessories/cachedAccessories')
+      cachedAccessories = JSON.parse(cachedAccessories)
+      cachedAccessories
+        .filter(accessory => accessory.plugin === 'homebridge-ewelink')
+        .sort((a, b) => {
+          return a.displayName.toLowerCase() > b.displayName.toLowerCase()
+            ? 1
+            : b.displayName.toLowerCase() > a.displayName.toLowerCase()
+              ? -1
+              : 0
+        })
+        .forEach(accessory => {
+          devicesToReturn.push({
+            displayName: accessory.displayName,
+            context: accessory.context
+          })
+        })
+      return devicesToReturn
+    })
+    this.ready()
+  }
+}
+
+(() => {
+  return new PluginUiServer()
+})()
